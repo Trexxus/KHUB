dc78606fea3be264a9f6a28f3370275158e0a54d
KR4SK <96661679+Trexxus@users.noreply.github.com>
2022-07-04 23:18:04 +0200

    Update KHUB.lua

--- KHUB.lua
+++ KHUB.lua
@@ -1,3 +1,4 @@
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/Trexxus/KHUB/main/KHUB.lua"))()
 if game.PlaceId == 189707 then
 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Trexxus/KHUB/main/NDSS.lua"))()
